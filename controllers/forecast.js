330db8feab44a4bc7c0a9373a47d3104625c7b22
Michael Hammer <michaelhammer@Michaels-MacBook-Pro.local>
2018-03-01 15:50:04 -0800

    post route

--- controllers/forecast.js
+++ controllers/forecast.js
@@ -1,7 +1,12 @@
 
 require('dotenv').config();
+var db = require('../models');
 var request = require('request');
 var express = require('express');
+var cloudinary = require('cloudinary');
+var multer = require('multer');
+var upload = multer({dest: './uploads/'});
+var fs = require('fs');
 var router = express.Router();
 
 
@@ -25,17 +30,65 @@ var router = express.Router();
 // });
 
 
-router.get('/:latlon', function(req, res) {
-  // var mt = req.params.latlon;
-  var snowInfo = 'http://api.wunderground.com/api/' + process.env.WUNDERGOUND_KEY + '/forecast/q/'+req.params.latlon+'.json';
-  request(snowInfo, function(error, response, body) {
-    if (!error, response, body) {
-      snowInfo = JSON.parse(body);
-      console.dir(snowInfo);
-      // res.send(snowInfo.forecast.simpleforecast);
-      res.render('forecast/weather', {snowInfo: snowInfo.forecast.simpleforecast.forecastday});
-    }
-  })
+router.get('/:resort', function(req, res) {
+  var resort = req.params.resort;
+  db.post.findAll({
+    where: { title: resort }
+  }).then(function(post) {
+    console.log(post);
+    var snowInfo = 'http://api.wunderground.com/api/' + process.env.WUNDERGOUND_KEY + '/forecast/q/'+resort+'.json';
+    request(snowInfo, function(error, response, body) {
+      if (!error, response, body) {
+        snowInfo = JSON.parse(body);
+        // console.dir(snowInfo);
+        // res.send(snowInfo.forecast.simpleforecast);
+        res.render('forecast/weather', {snowInfo: snowInfo.forecast.simpleforecast.forecastday, post: post, resort: resort });
+        }
+    })
+  });
 });
+
+
+
+router.post('/:resort/comment', upload.single('image'), function(req, res) {
+  console.log(req.body.image)
+  console.log(req.user.id)
+  if (req.body.image) {
+    cloudinary.v2.uploader.upload(req.file.path, function(error, result) {
+      console.log(db);
+      db.post.create({
+        title: req.body.title,
+        comment: req.body.comment,
+        image: result.url,
+        userId: req.user.id
+      }).then(function(post) {
+        fs.readdir('./uploads', function(err, items) {
+          items.forEach(function(file) {
+            fs.unlink('./uploads/' + file);
+            console.log('Deleted ' + file);
+          })
+        })
+        res.redirect('/forecast/weather');
+      })
+    });
+  } else {
+    db.post.create({
+      title: req.body.title,
+      comment: req.body.comment,
+      image: null,
+      userId: req.user.id
+    }).then(function(post) {
+      res.redirect('/forecast/weather');
+    })
+  }
+});
+
+
+
+
+
+//, {post: post}
+
+
 
 module.exports = router;
--- controllers/post.js
+++
@@ -1,4 +0,0 @@
-var express = require('express');
-var db = require('../models'); //allows access to data in models
-var passport = require('../config/ppConfig');
-var router = express.Router();
